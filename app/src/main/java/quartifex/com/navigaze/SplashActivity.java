abbd8f3989c57ee1b56a09cb7a4e2fc023eb7fd7
vigneshkumar <vigneshkumar@ccs.neu.edu>
2018-12-02 06:49:28 -0500

    added splash

--- app/src/main/java/quartifex/com/navigaze/SplashActivity.java
+++ app/src/main/java/quartifex/com/navigaze/SplashActivity.java
@@ -1,17 +1,56 @@
 package quartifex.com.navigaze;
 
+import android.app.AppComponentFactory;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends Activity {
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, FaceActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+
+
+
+	    ActionBar actionbar = getSupportActionBar();
+	   if(actionbar!=null){
+	   	actionbar.hide();
+	   }
+
+
+
+	    Window window = getWindow();
+
+	    // clear FLAG_TRANSLUCENT_STATUS flag:
+	    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+	    // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+	    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+	    // finally change the color
+	    window.setStatusBarColor(getResources().getColor(android.R.color.black));
+
+
+	    final Handler handler = new Handler();
+	    handler.postDelayed(new Runnable() {
+		    @Override
+		    public void run() {
+			    // Do something after 5s = 5000ms
+			    Intent intent = new Intent(SplashActivity.this, FaceActivity.class);
+			    startActivity(intent);
+			    finish();
+		    }
+	    }, 2000);
+
     }
 
 }
